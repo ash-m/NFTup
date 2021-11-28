a3655c96466cba9086964b2145374ea1c8a5d41c
istareatscreens <s.charb@gmail.com>
2021-11-27 19:48:28 -0500

    Add hardhat

---
+++ nftup/hardhat.config.js
@@ -0,0 +1,43 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env.rpc.local" });
+const fs = require("fs");
+
+const rpcId = process.env.rpcId;
+const privateKey = fs.readFileSync(".secret").toString();
+
+/*
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+*/
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**,f
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  paths: {
+    artifacts: "./assets/artifacts",
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${rpcId}`,
+      accounts: [privateKey],
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${rpcId}`,
+      accounts: [privateKey],
+    },
+  },
+};
